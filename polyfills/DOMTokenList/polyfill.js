1d76fa74e25b694afdb1cb955b4a9cf77e7fda46
Jake Champion <jakechampion.jake2@googlemail.com>
2017-09-27 22:51:05 +0100

    Improve DOMTokenList polyfill

--- polyfills/DOMTokenList/polyfill.js
+++ polyfills/DOMTokenList/polyfill.js
@@ -4,20 +4,25 @@
 	if (!nativeImpl) {
 		global.DOMTokenList = _DOMTokenList;
 	} else {
-		var NativeToggle = nativeImpl.prototype.toggle;
-
-		nativeImpl.prototype.toggle = function toggle(token) {
-			if (1 in arguments) {
-				var contains = this.contains(token);
-				var force = !!arguments[1];
-
-				if ((contains && force) || (!contains && !force)) {
-					return force;
+		// Add second argument to native DOMTokenList.toggle() if necessary
+		(function () {
+			if (!('DOMTokenList' in this)) return;
+			var e = document.createElement('span');
+			if (!('classList' in e)) return;
+			e.classList.toggle('x', false);
+			if (!e.classList.contains('x')) return;
+			this.DOMTokenList.prototype.toggle = function toggle(token /*, force*/ ) {
+				var force = arguments[1];
+				if (force === undefined) {
+					var add = !this.contains(token);
+					this[add ? 'add' : 'remove'](token);
+					return add;
 				}
-			}
-
-			return NativeToggle.call(this, token);
-		};
+				force = !!force;
+				this[force ? 'add' : 'remove'](token);
+				return force;
+			};
+		}())
 
 	}
 
--- polyfills/_DOMTokenList/polyfill.js
+++ polyfills/_DOMTokenList/polyfill.js
@@ -1,79 +1,171 @@
-var _DOMTokenList = (function () { // eslint-disable-line no-unused-vars
-
-	function tokenize(token) {
-		if (/^-?[_a-zA-Z]+[_a-zA-Z0-9-]*$/.test(token)) {
-			return String(token);
-		} else {
-			throw new Error('InvalidCharacterError: DOM Exception 5');
-		}
+function _DOMTokenList(o, p) {
+	function split(s) {
+		return s.length ? s.split(/\s+/g) : [];
 	}
 
-	function toObject(self) {
-		for (var index = -1, object = {}, element; element = self[++index];) {
-			object[element] = true;
+	// NOTE: This does not exactly match the spec.
+	function removeTokenFromString(token, string) {
+		var tokens = split(string);
+		var index = tokens.indexOf(token);
+		while (index !== -1) {
+			tokens.splice(index, 1);
+			index = tokens.indexOf(token);
 		}
-
-		return object;
+		return tokens.join(' ');
 	}
 
-	function fromObject(self, object) {
-		var array = [], token;
+	Object.defineProperties(
+		this, {
+			length: {
+				get: function () {
+					return split(o[p]).length;
+				}
+			},
 
-		for (token in object) {
-			if (object[token]) {
-				array.push(token);
+			item: {
+				value: function (idx) {
+					var tokens = split(o[p]);
+					return 0 <= idx && idx < tokens.length ? tokens[idx] : null;
+				}
+			},
+
+			contains: {
+				value: function (token) {
+					token = String(token);
+					if (token.length === 0) {
+						throw SyntaxError();
+					}
+					if (/\s/.test(token)) {
+						throw Error("InvalidCharacterError");
+					}
+					var tokens = split(o[p]);
+
+					return tokens.indexOf(token) !== -1;
+				}
+			},
+
+			add: {
+				value: function ( /*tokens...*/ ) {
+					var tokens = Array.prototype.slice.call(arguments).map(String);
+					if (tokens.some(function (token) {
+							return token.length === 0;
+						})) {
+						throw SyntaxError();
+					}
+					if (tokens.some(function (token) {
+							return (/\s/).test(token);
+						})) {
+						throw Error("InvalidCharacterError");
+					}
+
+					try {
+						var underlying_string = o[p];
+						var token_list = split(underlying_string);
+						tokens = tokens.filter(function (token) {
+							return token_list.indexOf(token) === -1;
+						});
+						if (tokens.length === 0) {
+							return;
+						}
+						if (underlying_string.length !== 0 && !(/\s$/).test(underlying_string)) {
+							underlying_string += ' ';
+						}
+						underlying_string += tokens.join(' ');
+						o[p] = underlying_string;
+					} finally {
+						var length = split(o[p]).length;
+						if (this.length !== length) {
+							this.length = length;
+						}
+					}
+				}
+			},
+
+			remove: {
+				value: function ( /*tokens...*/ ) {
+					var tokens = Array.prototype.slice.call(arguments).map(String);
+					if (tokens.some(function (token) {
+							return token.length === 0;
+						})) {
+						throw SyntaxError();
+					}
+					if (tokens.some(function (token) {
+							return (/\s/).test(token);
+						})) {
+						throw Error("InvalidCharacterError");
+					}
+
+					try {
+						var underlying_string = o[p];
+						tokens.forEach(function (token) {
+							underlying_string = removeTokenFromString(token, underlying_string);
+						});
+						o[p] = underlying_string;
+					} finally {
+						var length = split(o[p]).length;
+						if (this.length !== length) {
+							this.length = length;
+						}
+					}
+				}
+			},
+
+			toggle: {
+				value: function (token /*, force*/ ) {
+					var force = arguments[1];
+					try {
+						token = String(token);
+						if (token.length === 0) {
+							throw SyntaxError();
+						}
+						if (/\s/.test(token)) {
+							throw Error("InvalidCharacterError");
+						}
+						var tokens = split(o[p]),
+							index = tokens.indexOf(token);
+
+						if (index !== -1 && (!force || force === (void 0))) {
+							o[p] = removeTokenFromString(token, o[p]);
+							return false;
+						}
+						if (index !== -1 && force) {
+							return true;
+						}
+						var underlying_string = o[p];
+						if (underlying_string.length !== 0 && !/\s$/.test(underlying_string)) {
+							underlying_string += ' ';
+						}
+						underlying_string += token;
+						o[p] = underlying_string;
+						return true;
+					} finally {
+						var length = split(o[p]).length;
+						if (this.length !== length) {
+							this.length = length;
+						}
+					}
+				}
+			},
+
+			toString: {
+				value: function () {
+					return o[p];
+				}
 			}
+		});
+	if (!('length' in this)) {
+		// In case getters are not supported
+		this.length = split(o[p]).length;
+	} else {
+		// If they are, shim in index getters (up to 100)
+		for (var i = 0; i < 100; ++i) {
+			Object.defineProperty(this, String(i), {
+				get: (function (n) {
+					return function () {
+						return this.item(n);
+					};
+				}(i))
+			});
 		}
-
-		[].splice.apply(self, [0, self.length].concat(array));
 	}
-
-	var DTL = function() {};
-
-	DTL.prototype = {
-		constructor: DTL,
-		item: function item(index) {
-			return this[parseFloat(index)] || null;
-		},
-		length: Array.prototype.length,
-		toString: function toString() {
-			return [].join.call(this, ' ');
-		},
-
-		add: function add() {
-			for (var object = toObject(this), index = 0, token; index in arguments; ++index) {
-				token = tokenize(arguments[index]);
-
-				object[token] = true;
-			}
-
-			fromObject(this, object);
-		},
-		contains: function contains(token) {
-			return token in toObject(this);
-		},
-		remove: function remove() {
-			for (var object = toObject(this), index = 0, token; index in arguments; ++index) {
-				token = tokenize(arguments[index]);
-
-				object[token] = false;
-			}
-
-			fromObject(this, object);
-		},
-		toggle: function toggle(token) {
-			var
-			object = toObject(this),
-			contains = 1 in arguments ? !arguments[1] : tokenize(token) in object;
-
-			object[token] = !contains;
-
-			fromObject(this, object);
-
-			return !contains;
-		}
-	};
-
-	return DTL;
-
-}());
+}
